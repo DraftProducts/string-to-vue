b4a1cff54ce15cd976ded7376a73653cbb59f6ce
DraftProducts <nicovanaarsen@gmail.com>
2021-06-04 13:40:46 +0200

    build: 🔨 Update build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,19 @@ import buble from "@rollup/plugin-buble";
 import { terser } from "rollup-plugin-terser";
 export default {
   input: "src/wrapper.js",
-  external: ["vue"],
+  external: ["vue-template-compiler"],
   output: {
     name: "StringToVue",
     exports: "named",
     globals: {
-      vue: "Vue",
+      "vue-template-compiler": "VueTemplateCompiler",
     },
   },
   plugins: [
     commonjs(),
     vue({
       css: false, // Dynamically inject css as a <style> tag
-      compileTemplate: false, // Explicitly convert template to render function
+      compileTemplate: true, // Explicitly convert template to render function
     }),
     buble(), // Transpile to ES5
     terser(), // Minify bundle
